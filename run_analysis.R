254214f2d4a394fb4349721e7a519b06f0eef26e
Curtis Lin <curtiscjlin@outlook.com>
2015-09-27 16:24:53 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,17 @@
-#Establish path of files
-path <- "C:/Users/CHUN-JEN/Desktop/UCI HAR Dataset/"
-path_train <- "C:/Users/CHUN-JEN/Desktop/UCI HAR Dataset/train/"
-path_test <- "C:/Users/CHUN-JEN/Desktop/UCI HAR Dataset/test/"
+#Load feature.txt in /UCI HAR Dataset/ folder for column names
+feature <- read.table("./UCI HAR Dataset/features.txt", sep = "", stringsAsFactors = FALSE)
 
+#Load training data from /UCI HAR Dataset/train/ folder
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "", stringsAsFactors = FALSE)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", sep = "", stringsAsFactors = FALSE)
+sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", sep = "", stringsAsFactors = FALSE)
 
-#Load feature for column names
-setwd(path)
-feature <- read.table("features.txt", sep = "", stringsAsFactors = FALSE)
+#Load testing data from UCI HAR Dataset/test/ folder
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "", stringsAsFactors = FALSE)
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", sep = "", stringsAsFactors = FALSE)
+sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = "", stringsAsFactors = FALSE)
 
-#Load training data
-setwd(path_train)
-x_train <- read.table("X_train.txt", sep = "", stringsAsFactors = FALSE)
-y_train <- read.table("y_train.txt", sep = "", stringsAsFactors = FALSE)
-sub_train <- read.table("subject_train.txt", sep = "", stringsAsFactors = FALSE)
-
-#Load testing data
-setwd(path_test)
-x_test <- read.table("X_test.txt", sep = "", stringsAsFactors = FALSE)
-y_test <- read.table("y_test.txt", sep = "", stringsAsFactors = FALSE)
-sub_test <- read.table("subject_test.txt", sep = "", stringsAsFactors = FALSE)
-
-#create y-label
+#create y-label based on the six activities
 y_index <- rbind(y_train, y_test)
 colnames(y_index) <- "activity"
 y_index$activity[y_index$activity == 1] <- "WALKING"
@@ -31,17 +22,24 @@ y_index$activity[y_index$activity == 5] <- "STANDING"
 y_index$activity[y_index$activity == 6] <- "LAYING"
 
 
-#Merge training and testing set
+#Merges the training and the test sets to create one data set (Step 1) 
 merge_file <- rbind(x_train, x_test)
 colnames(merge_file) <- feature[,2]
 
-#subset mean and std data
+#Extracts only the measurements on the mean and standard deviation for each measurement (Step 2)
 mean_subset <- merge_file[grep("mean",names(merge_file),value=TRUE)]
 std_subset <- merge_file[grep("std",names(merge_file),value=TRUE)]
 
-#combine index, mean, and std data in one file
+#Uses descriptive activity names to name the activities in the data set (Step 3)
+#Appropriately labels the data set with descriptive variable names (Step 4)
 combine_data <- cbind(y_index, mean_subset, std_subset)
 
-#Calculate average of each variable for each activity
+#creates a second, independent tidy data set with the average of each variable for each activity and each subject (Step 5)
 mean_group <- aggregate(combine_data[, 2:80], list(combine_data$activity), mean)
 names(mean_group)[1] <- "Activity"
+
+#for creating txt file
+#write.table(mean_group, file = "mean_group.txt", sep = "\t", row.name=FALSE) 
+
+#for testing whether the second data is clean expressed
+#test <- read.table("mean_group.txt", sep = "", stringsAsFactors = FALSE, header = TRUE)
